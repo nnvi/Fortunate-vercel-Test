2999a27887687dda0faeaad3e43c3509ec023a0d
fndsjy <fendysanjaya40@gmail.com>
2024-05-23 16:36:27 +0700

    Modifying the DB Host in Database

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const {
   DB_USER = "postgres",
   DB_PASSWORD = "fendys",
   DB_NAME = "fortunate-coffee",
-  DB_HOST = "127.0.0.1",
+  DB_HOST = "localhost",
   DB_PORT = "5432",
 } = process.env;
 
